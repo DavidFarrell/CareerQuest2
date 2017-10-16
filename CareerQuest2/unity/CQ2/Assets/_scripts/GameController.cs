697ba28309e27bc3eb6955597d13e43eab9aef20
Unknown <dfarrell@davidlearnsgames.com>
2017-10-16 18:18:04 +0100

    better gitignore and a splash / homescreen

---
+++ CareerQuest2/unity/CQ2/Assets/_scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] panelList;
+
+	public GameObject splashPanel;
+	public GameObject homePanel;
+
+
+	public GameObject gameOverPanel;
+	public Text gameOverText;
+	public GameObject restartButton;
+
+
+	public void Awake() {
+		HideScreens (splashPanel);
+	}
+
+	public void HideScreens(GameObject activeScreen = null){ 
+		for (int i = 0; i < panelList.Length; i++) {
+			if ( panelList[i].Equals(activeScreen) ) {
+				panelList[i].SetActive(true);
+			} else {
+				panelList[i].SetActive (false);
+			}
+		}
+	}
+
+	public void handleInput( GameObject referer ) {
+		if (referer.Equals (splashPanel)) {
+			HideScreens (homePanel);
+		}
+	}
+}
